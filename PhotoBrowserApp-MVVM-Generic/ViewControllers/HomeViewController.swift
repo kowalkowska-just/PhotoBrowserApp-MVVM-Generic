01964cb8f38f4fa9f7044e5c622ae2d330e53718
Justyna <kowalkowska.just@gmail.com>
2021-03-03 22:47:09 +0100

    Setup project.

---
+++ PhotoBrowserApp-MVVM-Generic/ViewControllers/HomeViewController.swift
@@ -0,0 +1,28 @@
+//
+//  HomeViewController.swift
+//  PhotoBrowserApp-MVVM-Generic
+//
+//  Created by Justyna Kowalkowska on 03/03/2021.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemTeal
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
